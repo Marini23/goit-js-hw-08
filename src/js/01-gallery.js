2c8159e91ff82803ab023557c35f3bc616c80340
Marini23 <marina.smile1m@gmail.com>
2023-07-09 22:51:48 +0200

    task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,32 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryList = document.querySelector(`.gallery`);
+const itemsMarkup = creatItemsGalleryMarkup(galleryItems);
+
+
+function creatItemsGalleryMarkup(items) {
+return galleryItems.map(({ preview, original, description }) => {
+    return `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+</li>
+    `;
+    }).join(` `);
+}
+
+galleryList.insertAdjacentHTML(`beforeend`, itemsMarkup);
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
